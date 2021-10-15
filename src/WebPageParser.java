9a7fd6ab9b4ea80157f8a310c98a6120105904f9
unknown <potapovdenis09@gmail.com>
2021-10-15 21:00:16 +0300

    print hashmap of words counting

--- src/WebPageParser.java
+++ src/WebPageParser.java
@@ -1,28 +1,53 @@
 import java.io.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.PatternSyntaxException;
 
 class WebPageParser {
 
-    public static void DownloadWebPage(String webpage) throws PatternSyntaxException{
+    public static String DownloadWebPageText(String webpage) throws PatternSyntaxException {
         try {
 
             Document doc = Jsoup.connect(webpage).get();
             System.out.println("title - " + doc.title());
             String text = doc.body().text();
-            System.out.println("Text - " + text);
-            String[] words = text.split("[{',.!?%°\";:/|[',]()+\\d\\n\\r\\t\\h\\v\\s}]");
+            return text;
 
         } catch (PatternSyntaxException ie) {
             System.out.println("IOException raised:" + ie);
         } catch (IOException ie) {
             System.out.println("IOException raised:" + ie);
         }
+        return "";
+    }
+
+    public static void WordsCount(String text){
+        String[] words = text.split("[{',.!?%°©\";:/|«»[',]=()+\\d\\n\\r\\t\\h\\v\\s}]");
+        HashMap<String, Integer> wordsCount = new HashMap<>();
+
+        for (String word : words){
+            if (word.isEmpty() | word.equals("-")){
+                continue;
+            }
+            if (!wordsCount.containsKey(word)) {
+                wordsCount.put(word, 1);
+            } else {
+                wordsCount.put(word, wordsCount.get(word)+1);
+            }
+        }
+
+        for (Map.Entry entry: wordsCount.entrySet()) {
+            System.out.println(entry);
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
         String url = "http://www.yandex.ru";
-        DownloadWebPage(url);
+        String text = DownloadWebPageText(url);
+        WordsCount(text);
     }
 }
